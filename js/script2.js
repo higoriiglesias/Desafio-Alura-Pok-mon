098f97eed494e87dacb4fc3742ebda5241cfcc16
Higor <higoriiglesias@gmail.com>
2021-03-24 12:16:51 -0300

    first commit

---
+++ js/script2.js
@@ -0,0 +1,38 @@
+const pokemon001 = "Ivysaur";
+const botao = document.querySelector("button");
+const section = document.querySelector("section");
+
+function verificaPokemon() {
+    const h3 = document.querySelector("h3");
+    const chute = document.querySelector("input").value;
+
+    section.classList.add("active");
+    if (pokemon001 == chute) {
+        h3.textContent = "Certa!!";
+        h3.classList.remove("error");
+    } else {
+        h3.classList.add("error");
+        h3.textContent = "Errada!!";
+    }
+}
+
+function fechaModal() {
+    section.classList.remove("active");
+}
+
+function clicaForaFechaModal(event) {
+    console.log(event.target);
+
+    if (event.target.tagName == "section") {
+        section.classList.remove("active");
+    }
+}
+
+window.onload = function imageOption() {
+    document.getElementById("imageoption").src = "images/pokemon/002.png";
+}
+
+botao.onclick = verificaPokemon;
+
+section.querySelector("button").onclick = fechaModal;
+section.onclick = clicaForaFechaModal;
